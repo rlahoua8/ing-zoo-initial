24bf1d8a215ed47d48cbf14fe3568584b53fc1c3
lahouar <rlahoua2004@gmail.com>
2026-06-16 14:31:29 +0200

    feat: add Herbivore interface for plant-eating animals

---
+++ src/com/ing/zoo/interfaces/Herbivore.java
@@ -0,0 +1,11 @@
+package com.ing.zoo.interfaces;
+
+/**
+ * The Herbivore interface defines behavior for animals
+ * that eat plants. Any class implementing this interface
+ * must provide its own implementation of the eatLeaves() method.
+ */
+public interface Herbivore {
+    void eatLeaves();
+
+}
