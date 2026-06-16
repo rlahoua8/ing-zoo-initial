cdddf8c95c8497ac2c3dd70d6772c8db0848d3a2
lahouar <rlahoua2004@gmail.com>
2026-06-16 14:30:35 +0200

    add Carnivore interface for meat-eating animals

---
+++ src/com/ing/zoo/interfaces/Carnivore.java
@@ -0,0 +1,10 @@
+package com.ing.zoo.interfaces;
+
+/**
+ * The Carnivore interface defines behavior for animals
+ * that eat meat. Any animal that implements this interface
+ * must provide its own implementation of the eatMeat() method.
+ */
+public interface Carnivore {
+    void eatMeat();
+}
