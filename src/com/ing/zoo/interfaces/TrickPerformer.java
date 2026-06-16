901c678fdca7fae014359c06f751e61339b56cb8
lahouar <rlahoua2004@gmail.com>
2026-06-16 14:41:32 +0200

    feat: add TrickPerformer interface for animals that can perform tricks

---
+++ src/com/ing/zoo/interfaces/TrickPerformer.java
@@ -0,0 +1,10 @@
+package com.ing.zoo.interfaces;
+
+/**
+ * The TrickPerformer interface defines behavior for animals
+ * that can perform simple tricks. Any class implementing this
+ * interface must provide its own implementation of the performTrick() method.
+ */
+public interface TrickPerformer {
+    void performTrick();
+}
